fdbdc230f45eceee163b50a20cbf37bd2eac6cf8
hbjpn <tokyochofu@gmail.com>
2013-04-01 18:18:58 +0900

    Modify header file ..^^

--- strutil.h
+++ strutil.h
@@ -17,10 +17,12 @@
  *
  */
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "strutil.h"
+#ifndef STRUTIL_H
+#define STRUTIL_H
+
+#include <string>
+#include <vector>
+
 
 namespace strutil
 {
@@ -31,14 +33,7 @@ namespace strutil
  * @param s Input string
  * @return Trimed string
  */
-std::string rtrim(const std::string& s)
-{
-    int i = s.size() - 1;
-
-    while ( i >= 0 && ( s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == '\r') ) i--;
-
-    return s.substr(0, i+1);
-}
+std::string rtrim(const std::string& s);
 
 /**
  * Remove white space, tab, \n and \r from left side of the specified string
@@ -46,13 +41,7 @@ std::string rtrim(const std::string& s)
  * @param s Input string
  * @return Trimed string
  */
-std::string ltrim(const std::string& s)
-{
-    int i = 0;
-    while ( i < s.size() && ( s[i] == ' ' || s[i] == '\t'  || s[i] == '\n' || s[i] == '\r') ) i++;
-
-    return s.substr(i, s.size() - i);
-}
+std::string ltrim(const std::string& s);
 
 /**
  * Remove white space, tab, \n and \r from both right and left sides of the specified string
@@ -60,10 +49,7 @@ std::string ltrim(const std::string& s)
  * @param s Input string
  * @return Trimed string
  */
-std::string trim(const std::string& s)
-{
-	return ltrim(rtrim(s));
-}
+std::string trim(const std::string& s);
 
 /**
  * Join specified strings with delimiter
@@ -73,28 +59,7 @@ std::string trim(const std::string& s)
  * @param Size of s
  * @return Joined string
  */
-std::string join(const std::string& delim, const std::string* s, int size)
-{
-	int retLen = 0;
-	for(int i = 0; i < size; ++i)
-	{
-		retLen += s[i].size();
-	}
-	retLen += delim.size() * (size-1);
-
-	std::string ret;
-	ret.reserve(retLen);
-
-	for(int i = 0; i < size; ++i)
-	{
-		ret += s[i];
-		if( i < size-1 )
-		{
-			ret += delim;
-		}
-	}
-	return ret;
-}
+std::string join(const std::string& delim, const std::string* s, int size);
 
 /**
  * Join specified strings with delimiter
@@ -104,30 +69,20 @@ std::string join(const std::string& delim, const std::string* s, int size)
  * @param Size of s
  * @return Joined string
  */
-std::string join(const std::string& delim, const std::vector<std::string>& s)
-{
-	return join(delim, &s[0], s.size());
-}
-
+std::string join(const std::string& delim, const std::vector<std::string>& s);
 /**
  * Test if s0 is contained in s1
  *
  * return true if contained, otherwise false
  */
-bool contained(const std::string& s0, const std::string& s1)
-{
-	return std::string::npos != s1.find(s0, 0);
-}
+bool contained(const std::string& s0, const std::string& s1);
 
 /**
  * Test if c is contained in s1
  *
  * return true if contained, otherwise false
  */
-bool contained(char c, const std::string& s1)
-{
-	return std::string::npos != s1.find(c, 0);
-}
+bool contained(char c, const std::string& s1);
 
 /**
  * Split specified string with specified delimiter
@@ -138,123 +93,21 @@ bool contained(char c, const std::string& s1)
  *
  * @note Each characters in delim is considered as delimiter character
  */
-std::vector<std::string> split(const std::string& s, const std::string& delim)
+std::vector<std::string> split(const std::string& s, const std::string& delim);
+
+struct parsecontext
 {
-    std::vector<std::string> strings;
-    std::string tmp;
+	int i;
+	std::string tmp;
+};
 
-    for(int i = 0; i < s.size(); ++i)
-    {
-        if(contained(s[i], delim))
-        {
-            if(tmp.length() != 0)
-            {
-                strings.push_back(tmp);
-            }
-            tmp = "";
-        }else
-        {
-            tmp += s[i];
-        }
-    }
+static void split_ibs0(parsecontext& pc, const std::string& s, const std::string& delim, char quote, std::vector<std::string>& ret);
 
-    if(tmp.length() != 0)
-    {
-        strings.push_back(tmp);
-    }
+static  void split_ibs1(parsecontext& pc, const std::string& s, const std::string& delim, char quote, std::vector<std::string>& ret);
 
-    return strings;
-}
+static void split_inq(parsecontext& pc, const std::string& s, const std::string& delim, char quote, std::vector<std::string>& ret);
 
-static void split_ibs0(parsecontext& pc, const std::string& s, const std::string& delim, char quote, std::vector<std::string>& ret)
-{
-	if(pc.i == s.size())
-	{
-		throw "Parse error";
-	}
-	else if(s[pc.i] == quote || s[pc.i] == '\\' || contained(s[pc.i], delim))
-	{
-		pc.tmp += s[pc.i];
-		pc.i++;
-		split_st(pc, s, delim, quote, ret);
-	}else
-	{
-		throw "Parse error";
-	}
-}
-
-static  void split_ibs1(parsecontext& pc, const std::string& s, const std::string& delim, char quote, std::vector<std::string>& ret)
-{
-	if(pc.i == s.size())
-	{
-		throw "Parse error";
-	}
-	else if(s[pc.i] == quote || s[pc.i] == '\\' || contained(s[pc.i], delim))
-	{
-		pc.tmp += s[pc.i];
-		pc.i++;
-		split_inq(pc, s, delim, quote, ret);
-	}else
-	{
-		throw "Parse error";
-	}
-}
-
-
-static void split_inq(parsecontext& pc, const std::string& s, const std::string& delim, char quote, std::vector<std::string>& ret)
-{
-	if(pc.i == s.size())
-	{
-		throw "Parse error";
-	}else if(s[pc.i] == '\\')
-	{
-		pc.i++;
-		split_ibs1(pc, s, delim, quote, ret);
-	}else if(s[pc.i] == quote)
-	{
-		pc.i++;
-		split_st(pc, s, delim, quote, ret);
-	}else
-	{
-		pc.tmp += s[pc.i];
-		pc.i++;
-		split_inq(pc, s, delim, quote, ret);
-	}
-}
-
-static void split_st(parsecontext& pc, const std::string& s, const std::string& delim, char quote, std::vector<std::string>& ret)
-{
-    if( pc.i == s.size() || contained(s[pc.i], delim) )
-    {
-		if(pc.tmp.length() != 0)
-		{
-			ret.push_back(pc.tmp);
-		}
-		pc.tmp = "";
-		if(pc.i == s.size())
-		{
-			// Parse end
-			return;
-		}else
-		{
-			pc.i++;
-			split_st(pc, s, delim, quote, ret);
-		}
-    }else if(s[pc.i] == '\\')
-    {
-    	pc.i++;
-    	split_ibs0(pc, s, delim, quote, ret);
-    }else if(s[pc.i] == quote)
-    {
-    	pc.i++;
-    	split_inq(pc, s, delim, quote, ret);
-    }else
-    {
-    	pc.tmp += s[pc.i];
-    	pc.i++;
-    	split_st(pc, s, delim, quote, ret);
-    }
-}
+static void split_st(parsecontext& pc, const std::string& s, const std::string& delim, char quote, std::vector<std::string>& ret);
 
 /**
  * Split specified string with specified delimiter considering quote symbol
@@ -266,14 +119,8 @@ static void split_st(parsecontext& pc, const std::string& s, const std::string& 
  *
  * @note Each characters in delim is considered as delimiter character
  */
-std::vector<std::string> split(const std::string& s, const std::string& delim, char quote)
-{
-	std::vector<std::string> ret;
-	parsecontext pc;
-	pc.i = 0;
-	split_st(pc, s, delim, quote, ret);
-	return ret;
-}
+std::vector<std::string> split(const std::string& s, const std::string& delim, char quote);
+
 
 /**
  * Convert string to double
@@ -283,21 +130,7 @@ std::vector<std::string> split(const std::string& s, const std::string& delim, c
  *
  * @return Converted value if succeed, otherwise 0
  */
-double toDouble(const std::string& s, bool& success)
-{
-    char* e;
-    double v = strtod(s.c_str(), &e);
-
-    if(*e == '\0')
-    {
-        success = true;
-    }else
-    {
-        success = false;
-    }
-
-    return v;
-}
+double toDouble(const std::string& s, bool& success);
 
 
 /**
@@ -308,21 +141,7 @@ double toDouble(const std::string& s, bool& success)
  *
  * @return Converted value if succeed, otherwise 0
  */
-int toInt(const std::string& s, bool& success)
-{
-    char* e;
-    int v = strtol(s.c_str(), &e, 10);
-
-    if(*e == '\0')
-    {
-        success = true;
-    }else
-    {
-        success = false;
-    }
-
-    return v;
-}
+int toInt(const std::string& s, bool& success);
 
 
 /**
@@ -333,12 +152,7 @@ int toInt(const std::string& s, bool& success)
  *
  * @return Formatted string
  */
-std::string format(const std::string& f, double v)
-{
-	char buf[128];
-	snprintf(buf, sizeof(buf), f.c_str(), v);
-	return std::string(buf);
-}
+std::string format(const std::string& f, double v);
 
 /**
  * Format string of integer value
@@ -348,12 +162,7 @@ std::string format(const std::string& f, double v)
  *
  * @return Formatted string
  */
-std::string format(const std::string& f, int v)
-{
-	char buf[128];
-	snprintf(buf, sizeof(buf), f.c_str(), v);
-	return std::string(buf);
-}
+std::string format(const std::string& f, int v);
 
 /**
  * Convert double value to string
@@ -362,10 +171,7 @@ std::string format(const std::string& f, int v)
  *
  * @return String
  */
-std::string toString(double v)
-{
-	return format("%f", v);
-}
+std::string toString(double v);
 
 /**
  * Convert integer value to string
@@ -374,26 +180,10 @@ std::string toString(double v)
  *
  * @return String
  */
-std::string toString(int v)
-{
-	return format("%d", v);
-}
+std::string toString(int v);
 
 
 } // namespace strutil
 
-
-#if 1
-#include <iostream>
-int main(int argc, char** argv)
-{
-	char in[1024];
-	std::cin.getline(in, sizeof(in));
-	std::vector<std::string> ret = strutil::split(in, ",", '\"');
-	std::cout << "input:[" << in << "]" << std::endl;
-	for(int i = 0; i < ret.size(); ++i)
-	{
-		std::cout << i << " : " << ret[i] << std::endl;
-	}
-}
 #endif
+
